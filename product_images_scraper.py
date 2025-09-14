7f1fa89503214711b871a739383e47907c686a08
yash <thevoiceofyashbhamare@gmail.com>
2025-09-15 03:11:39 +0530

    added scraping for product images

--- product_images_scraper.py
+++ product_images_scraper.py
@@ -15,7 +15,7 @@ class CONFIG:
     num_threads : int = 4
 
 
-url = 'https://www.amazon.in/Whirlpool-Refrigerator-205-WDE-CLS/dp/B0BSRVL2VV'
+url = 'https://www.amazon.in/Sunfeast-Yumfills-Whoopie-Chocolate-Chip/dp/B06WGM2HK2'
 
 
 def driver_setup() -> webdriver.Chrome: 
@@ -23,42 +23,50 @@ def driver_setup() -> webdriver.Chrome:
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--window-size=1920,1080") 
+    options.add_argument("--start-maximized")
     
     driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options = options)
     return driver
 
 
 def product_image(driver, url :str, thread : int) -> str | None:
-    try:
-        driver.get(url) 
-        view_more= WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "a-declarative")))
-        view_more.click()
+    driver.get(url) 
+    view_more= WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.LINK_TEXT, "Click to see full view")))
+    view_more.click()
 
-        thumb = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, f'ivImage_{thread}')))
+    try:
+        thumb = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, f'ivImage_{thread}')))
         thumb.click()
+        time.sleep(2)
 
         img = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "ivLargeImage")))
         img_url = img.find_element(By.TAG_NAME, 'img').get_attribute('src')
-        driver.quit()
-        return img_url
-
     except Exception as e:
-        return None
+        img_url = None
+
+    driver.quit()
+    return img_url
 
 
 def main():
-    config = CONFIG(url = url, num_threads= 4)
-    drivers = [driver_setup() for _ in range(config.num_threads)]
+    config = CONFIG(url = url, num_threads= 8)
 
-    start = time.perf_counter()
+    starti = time.perf_counter()
+    drivers = [driver_setup() for _ in range(config.num_threads)]
+    endi = time.perf_counter()
+
+
+    startp = time.perf_counter()
     with ThreadPoolExecutor(max_workers= config.num_threads) as excecutor:
         futures = [excecutor.submit(product_image, drivers[i], config.url, i) for i in range(config.num_threads)]
 
         for future in futures:
             print(future.result())
+    endp = time.perf_counter()
 
-    end = time.perf_counter()
-    print(f"TIME TAKEN: {end - start}")
+    print(f"INITIALIZATION TIME: {endi - starti}")
+    print(f"PROCESSING TIME: {endp-startp}")
 
 if __name__ == "__main__":
     main()
